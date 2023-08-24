36d754d46373ef73f8807e0f7a5b108454a71c11
2611saurabh <saurabh.kr026@gmail.com>
2023-08-24 16:41:19 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,121 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
+
+
 function App(){
 
-    return <h1>Hello React</h1>;
+    return (
+        <div>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
         
 }
 
+function Header(){
 
+  return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu(){
+
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+
+}
+
+function Footer(){
+  // return React.createElement("footer", null, "We're currently open!");
+  const hour = new Date().getHours();
+  //console.log(hour);
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour
+
+  // if(hour >= openHour && hour <= closeHour)
+  // {
+  //   alert("we'are currently open")
+  // }
+  // else{
+  //   alert("Soory we are closed"); 
+  // }
+
+
+  return <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
+
+
+}
+
+
+
+
+function Pizza() {
+    return (
+        <div>
+        <img src="pizzas/spinaci.jpg" alt="" />
+        <h2>Pizza Spinaci</h2>
+        <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
+    </div>
+    )
+    
+    
+}
 
 
 
